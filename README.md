e4308626f977b1709541cb18b3c320e9b751f3ec
Tugce Aras <tugce20aras@hotmaail.com>
2021-10-11 18:52:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# **Basic Alert Dialog**
+<br/>
+
+
+## Why Do We Use?
+
+<font size="3">We use it when we want to show an information message or an error message to the user. The user sees a pop-up image on the screen. </font>
+
+<br/><br/>
+
+  ![](https://drive.google.com/uc?export=view&id=1OSnSaKQjJuZ_70WtTfy8Sfcu5j5BkZKs)
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/tugcearas/alertdialog/MainActivity.java
@@ -0,0 +1,73 @@
+package com.tugcearas.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+
+            CONTEXT : App içinde neler olduğunu , hangi durumların var olduğunu,... gibi olayların tutulduğu yapı.
+             
+            1) Activity Context
+                this ya da MainActivity.this kullanılır
+
+            2) App Context
+                getApplicationContext() kullanılır
+
+
+            ne zaman hangisi kullanılır ?
+                Genel app' i ilgilendiren konularda App Context kullanılır. Diğer durumlarda Activity Context kullanılır.
+
+        */
+
+
+
+        // küçük pop-up mesaj oluşturmak için ;
+        Toast.makeText(MainActivity.this,"Toast message",Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public void save(View view){
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // save
+                Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
+
+
+    }
+
+
+}
